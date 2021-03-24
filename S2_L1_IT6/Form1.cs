473f560fd4266d37b8462e406e97f4d06704b879
ddatsenko <58321633+ddatsenko@users.noreply.github.com>
2021-03-24 10:43:02 +0200

    Після того, як всі кнопки вже зникли з вікна, нічого не відбувається

--- S2_L1_IT6/Form1.cs
+++ S2_L1_IT6/Form1.cs
@@ -12,9 +12,27 @@ namespace S2_L1_IT6
 {
     public partial class Form1 : Form
     {
+        List<Button> buttons;
         public Form1()
         {
             InitializeComponent();
+            buttons = new List<Button>();
+            buttons.AddRange(new Button[5] 
+            { 
+                button1, button2, button3, button4, button5 
+            });
+            button1.Click += button1_Click;
+            button2.Click += button1_Click;
+            button3.Click += button1_Click;
+            button4.Click += button1_Click;
+            button5.Click += button1_Click;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rndm = new Random(DateTime.Now.Millisecond);
+            Button bttn = buttons[rndm.Next(buttons.Count)];
+            bttn.Visible = !bttn.Visible;
         }
     }
 }
